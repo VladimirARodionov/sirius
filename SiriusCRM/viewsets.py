c6cc9744d60675b6bfe4ec496c3f7eb140dac1ad
Vladimir Rodionov <vladimirarodionov@gmail.com>
2018-12-03 19:00:25 +0300

    search users

--- SiriusCRM/viewsets.py
+++ SiriusCRM/viewsets.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from rest_framework import viewsets
+from django.core.paginator import Paginator
+from rest_framework import viewsets, filters
 
 from SiriusCRM.models import User
 from SiriusCRM.serializers import UserListSerializer, UserDetailSerializer
@@ -8,6 +9,8 @@ from SiriusCRM.serializers import UserListSerializer, UserDetailSerializer
 class UserListViewSet(LoginRequiredMixin, viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserListSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('first_name', 'last_name', 'email')
 
 
 class UserDetailViewSet(LoginRequiredMixin, viewsets.ModelViewSet):
