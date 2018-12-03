f9463c60120245d5226aaf32697f81121672e678
Vladimir Rodionov <vladimirarodionov@gmail.com>
2018-12-03 22:00:36 +0300

    add comments to model set user email as nullable generate requirements.txt

--- SiriusCRM/models.py
+++ SiriusCRM/models.py
@@ -9,21 +9,25 @@ from django.utils.translation import ugettext_lazy as _
 from SiriusCRM.managers.UserManager import UserManager
 
 
+# Справочник стран
 class Country(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, null=False, blank=False)
 
 
+# Справочник регионов
 class Region(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, null=False, blank=False)
 
 
+# Справочник городов
 class City(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, null=False, blank=False)
 
 
+# Справочник адресов
 class Address(models.Model):
     id = models.AutoField(primary_key=True)
     country = models.ForeignKey(Country, null=True, on_delete=models.CASCADE)
@@ -35,55 +39,66 @@ class Address(models.Model):
     apartment = models.CharField(max_length=10, null=True, blank=True)
 
 
+# Имя и адрес социальной сети
 class SocialName(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, blank=False)
     address = models.CharField(max_length=255, blank=False)
 
 
+# Аккаунт в социальной сети. Name берется из справочника SocialName
 class Social(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.ForeignKey(SocialName, null=False, on_delete=models.CASCADE)
     account = models.CharField(max_length=255, blank=False)
 
 
+# Справочник организаций
 class Organization(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, blank=False)
     address = models.ForeignKey(Address, null=True, on_delete=models.CASCADE)
 
 
+# Справочник подразделений организации. Если parent != null то это подразделение вложено в parent
 class Unit(models.Model):
     id = models.AutoField(primary_key=True)
+    organization = models.ForeignKey(Organization, null=False, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, blank=False)
     parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
 
 
+# Справочник названий позиций человека (должность)
 class Position(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, blank=False)
 
 
+# Справочник названий категорий человека (ученик, сотрудник,...)
 class Category(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, blank=False)
 
 
+# Справочник названий курсов
 class Course(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, blank=False)
 
 
+# Справочник эссе человека. Находится по таблице UserCourseEssay
 class Essay(models.Model):
     id = models.AutoField(primary_key=True)
     essay = models.TextField(blank=False)
 
 
+# Справочник компетенций человека, что он умеет
 class Competency(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, blank=False)
 
 
+# Справочник offline мероприятий. Адрес берется из справочника адресов
 class Offline(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, blank=False)
@@ -92,17 +107,20 @@ class Offline(models.Model):
     date_end = models.DateField(null=True, blank=True)
 
 
+# Справочник названий выплат
 class Payment(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, blank=False)
 
 
+# Справочник валют
 class Currency(models.Model):
     id = models.AutoField(primary_key=True)
     short_name = models.CharField(max_length=3, blank=False)
     name = models.CharField(max_length=255, blank=False)
 
 
+# Список контактов, лидов
 class Contact(models.Model):
     id = models.AutoField(primary_key=True)
     email = models.EmailField(_('email address'), unique=True)
@@ -112,8 +130,9 @@ class Contact(models.Model):
     socials = models.ManyToManyField(Social, through='ContactSocial')
 
 
+# Список пользователей
 class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(_('email address'), unique=True)
+    email = models.EmailField(_('email address'), unique=True, null=True)
     first_name = models.CharField(_('first name'), max_length=50, blank=False)
     last_name = models.CharField(_('last name'), max_length=50, blank=False)
     middle_name = models.CharField(_('middle name'), max_length=50, blank=True)
@@ -167,6 +186,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         send_mail(subject, message, from_email, [self.email], **kwargs)
 
 
+# Таблица связей пользователя и его позиции
 class UserPosition(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     position = models.ForeignKey(Position, on_delete=models.CASCADE)
@@ -174,6 +194,7 @@ class UserPosition(models.Model):
     invite_reason = models.CharField(max_length=255, null=True, blank=True)
 
 
+# Таблица связей пользователя и его категории
 class UserCategory(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -181,6 +202,7 @@ class UserCategory(models.Model):
     invite_reason = models.CharField(max_length=255, null=True, blank=True)
 
 
+# Таблица связей пользователя и курсов
 class UserCourse(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
@@ -188,6 +210,7 @@ class UserCourse(models.Model):
     date_end = models.DateField(null=True, blank=True)
 
 
+# Таблица связей пользователя, курсов и его эссе
 class UserCourseEssay(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
@@ -195,24 +218,28 @@ class UserCourseEssay(models.Model):
     date = models.DateField(null=True, blank=True)
 
 
+# Таблица связей курсов и их кураторов
 class CourseCurator(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     course = models.ForeignKey(Position, on_delete=models.CASCADE)
     is_primary = models.BooleanField(default=False)
 
 
+# Таблица связей пользователя и offline мероприятия, в котором он участвовал
 class UserOffline(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     offline = models.ForeignKey(Offline, on_delete=models.CASCADE)
 
 
+# Таблица связей пользователя и его компетенций
 class UserCompetency(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     competency = models.ForeignKey(Competency, on_delete=models.CASCADE)
 
 
-class UserPayment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+# Таблица прихода денежных средств в организацию
+class OrganizationIncome(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
     currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
     date = models.DateField(null=False, blank=False)
@@ -220,8 +247,9 @@ class UserPayment(models.Model):
     note = models.CharField(max_length=255, null=True, blank=True)
 
 
-class OrganizationPayment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+# Таблица расхода денежных средств из организации
+class OrganizationOutcome(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
     currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
     date = models.DateField(null=False, blank=False)
@@ -229,11 +257,13 @@ class OrganizationPayment(models.Model):
     note = models.CharField(max_length=255, null=True, blank=True)
 
 
+# Таблица связей пользователя и его аккаунтов в социальных сетях
 class UserSocial(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     social = models.ForeignKey(Social, on_delete=models.CASCADE)
 
 
+# Таблица связей контакта (лида) и его аккаунтов в социальных сетях
 class ContactSocial(models.Model):
     contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
     social = models.ForeignKey(Social, on_delete=models.CASCADE)
