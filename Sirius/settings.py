365b09308996037a49b0de0d3308312a40c37c32
Vladimir Rodionov <vladimirarodionov@gmail.com>
2018-12-06 14:09:35 +0300

    update translation file

--- Sirius/settings.py
+++ Sirius/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+from django.utils.translation import gettext_lazy as _
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -128,6 +130,8 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-LOCALE_PATHS = (
-    BASE_DIR + 'SiriusCRM/locale', )
+LOCALE_PATHS = (BASE_DIR + '/SiriusCRM/locale', )
 AUTH_USER_MODEL = 'SiriusCRM.User'
+
+LANGUAGES = [('en', _('English')), ('ru', _('Russian')), ]
+TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, 'templates'), 'templates' )
