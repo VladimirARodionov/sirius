2237f3e1ab3609c0add2cbf7c4a3cc00629f93b4
Vladimir Rodionov <vladimirarodionov@gmail.com>
2018-12-12 13:15:46 +0300

    add localization vuex.i18n

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,21 +2,21 @@
   <div id="app">
     <span v-if="isLoggedIn">
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-      <router-link class="navbar-brand" active-class="active" to="/">Sirius</router-link>
+      <router-link class="navbar-brand" active-class="active" to="/">{{'Sirius' | translate}}</router-link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav d-lg-flex align-items-center">
-          <router-link class="nav-item nav-link" active-class="active" to="/people">People</router-link>
-          <router-link class="nav-item nav-link" active-class="active" to="/reports">Reports</router-link>
-          <router-link class="nav-item nav-link" active-class="active" to="/actions">Actions</router-link>
-          <router-link class="nav-item nav-link" active-class="active" to="/directories">Directories</router-link>
+          <router-link class="nav-item nav-link" active-class="active" to="/people">{{'People' | translate}}</router-link>
+          <router-link class="nav-item nav-link" active-class="active" to="/reports">{{'Reports' | translate}}</router-link>
+          <router-link class="nav-item nav-link" active-class="active" to="/actions">{{'Actions' | translate}}</router-link>
+          <router-link class="nav-item nav-link" active-class="active" to="/directories">{{'Directories' | translate}}</router-link>
         </div>
         <div class="navbar-nav ml-auto d-lg-flex align-items-center">
           <div class="nav-item">
-            <a class="nav-link" @click="logout"><span class="fa fa-user"></span>Logout</a>
+            <a class="nav-link" @click="logout"><span class="fa fa-user"></span>{{'Logout' | translate}}</a>
           </div>
         </div>
       </div>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Axios from 'axios'
+import vuexI18n from 'vuex-i18n'
 
 Vue.prototype.$http = Axios
 
@@ -12,6 +13,19 @@ if (token) {
 }
 
 Vue.config.productionTip = false
+
+Vue.use(vuexI18n.plugin, store,
+  {
+    moduleName: 'i18n',
+    onTranslationNotFound (locale, key) {
+      console.warn(`i18n :: Key '${key}' not found for locale '${locale}'`)
+    }
+  }
+)
+
+Vue.i18n.add('ru', require('./locale/ru.json'))
+
+Vue.i18n.set('ru')
 
 new Vue({
   router,
