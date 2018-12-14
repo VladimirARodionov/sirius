410bd763ec0e8c11249215ffeb605763d57f13d1
Vladimir Rodionov <vladimirarodionov@gmail.com>
2018-12-14 15:11:00 +0300

    minor warning changes

--- frontend/src/components/PeopleDetails.vue
+++ frontend/src/components/PeopleDetails.vue
@@ -43,7 +43,6 @@
 
 <script>
 import axios from 'axios'
-import router from '../router'
 
 export default {
   name: 'PeopleDetails',
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,7 +9,6 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-
 library.add(faUser)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
